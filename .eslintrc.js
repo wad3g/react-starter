32ebd35599a1964c2a1a9f0b49d0ecf73edfbfc6
David Livingston <livingston.david.w@gmail.com>
2018-01-05 19:45:51 -0600

    add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+// can't extend this further without ejecting
+// https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md#displaying-lint-output-in-the-editor
+
+module.exports = {
+  extends: 'react-app',
+};
